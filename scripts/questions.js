abbe930efcc61ab188d634e6a9339d0fdc396a1b
Mikko Pasanen <pasanemikko@gmail.com>
2023-11-06 12:59:04 +0200

    Initialize project and import existing files

---
+++ scripts/questions.js
@@ -0,0 +1,217 @@
+const questions = [
+    {
+        question: 'What does "Mise en place" mean?',
+        answers: [
+            {
+                text: "Restaurant where the owner is the chef",
+                correct: "false",
+            },
+            {
+                text: "The area where all pots and pans are stored",
+                correct: "false",
+            },
+            {
+                text: "The preparation and organisation of ingridients before cooking",
+                correct: "true",
+            },
+            { text: "Traditional french dessert", correct: "false" },
+        ],
+        img: "./pics/mise-en-place.webp",
+    },
+    {
+        question:
+            "What is the Italian term for pasta that is cooked until is just tender to the bite?",
+        answers: [
+            {
+                text: "Al dente",
+                correct: "true",
+            },
+            {
+                text: "Al forno",
+                correct: "false",
+            },
+            {
+                text: "Al pomodoro",
+                correct: "false",
+            },
+            { text: "Al pesto", correct: "false" },
+        ],
+        img: "./pics/al-dente.webp",
+    },
+    {
+        question:
+            "What fruit is the primary ingredient in a classic French tarte Tatin?",
+        answers: [
+            {
+                text: "Cherry",
+                correct: "false",
+            },
+            {
+                text: "Apple",
+                correct: "true",
+            },
+            {
+                text: "Peach",
+                correct: "false",
+            },
+            { text: "Mango", correct: "false" },
+        ],
+        img: "./pics/tarte-tatin.webp",
+    },
+    {
+        question: 'What does it mean to "cream" the butter and sugar',
+        answers: [
+            {
+                text: "Mix them together until smooth",
+                correct: "false",
+            },
+            {
+                text: "Beat them until light and fluffy",
+                correct: "true",
+            },
+            {
+                text: "Heat them to a liquid state",
+                correct: "false",
+            },
+            { text: "Add cream to the mixture", correct: "false" },
+        ],
+        img: "./pics/cream.webp",
+    },
+    {
+        question:
+            "Which type of sugar is oftern used for making a glossy caramel topping on desserts?",
+        answers: [
+            {
+                text: "Granulated sugar",
+                correct: "false",
+            },
+            {
+                text: "Powdered sugar",
+                correct: "false",
+            },
+            {
+                text: "Caster sugar",
+                correct: "false",
+            },
+            { text: "Demerara sugar", correct: "true" },
+        ],
+        img: "pics/sugars.webp",
+    },
+    {
+        question: "What gives croissants their characteristic flaky texture?",
+        answers: [
+            {
+                text: "Baking powder",
+                correct: "false",
+            },
+            {
+                text: "Layers of butter and dough",
+                correct: "true",
+            },
+            {
+                text: "Yeast",
+                correct: "false",
+            },
+            { text: "Milk", correct: "false" },
+        ],
+        img: "pics/croissants.webp",
+    },
+    {
+        question: "What is traditionally the butchers cut of beef?",
+        answers: [
+            {
+                text: "Hanger",
+                correct: "true",
+            },
+            {
+                text: "Entrecote",
+                correct: "false",
+            },
+            {
+                text: "Flank",
+                correct: "false",
+            },
+            { text: "Loin", correct: "false" },
+        ],
+        img: "pics/butchers-cut.webp",
+    },
+    {
+        question: "How many in a bakers dozen?",
+        answers: [
+            {
+                text: "12",
+                correct: "false",
+            },
+            {
+                text: "11",
+                correct: "false",
+            },
+            {
+                text: "13",
+                correct: "true",
+            },
+            { text: "16", correct: "false" },
+        ],
+        img: "pics/icon.webp",
+    },
+    {
+        question: "What chef ranks between a Sous Chef and a Commie Chef?",
+        answers: [
+            {
+                text: "Pastry Chef",
+                correct: "false",
+            },
+            {
+                text: "Chef de Cuisine",
+                correct: "false",
+            },
+            {
+                text: "Chef de Partie",
+                correct: "true",
+            },
+            { text: "Executive Chef", correct: "false" },
+        ],
+        img: "pics/icon.webp",
+    },
+    {
+        question: "What kind of pasta is this?",
+        answers: [
+            {
+                text: "Spagetti",
+                correct: "false",
+            },
+            {
+                text: "Penne",
+                correct: "false",
+            },
+            {
+                text: "Tortelloni",
+                correct: "false",
+            },
+            { text: "Linguine", correct: "true" },
+        ],
+        img: "pics/icon.webp",
+    },
+    {
+        question:
+            "What cooking technique involves briefly immersing food in boiling water and then immediately placing it in ice?",
+        answers: [
+            {
+                text: "Braising",
+                correct: "false",
+            },
+            {
+                text: "Steaming",
+                correct: "false",
+            },
+            {
+                text: "Blanching",
+                correct: "true",
+            },
+            { text: "Searing", correct: "false" },
+        ],
+        img: "pics/icon.webp",
+    },
+];
+
+export { questions };
---
+++ scripts/quiz.js
@@ -0,0 +1,146 @@
+/*
+TODO:
+
+Värimaailma
+Kaikkiin kysymyksiin kuvat
+Lisää 4 kysymystä
+Aloitusruutu
+Lopetusruutu
+Mobile first
+Lajittele scss tiedostot paremmin
+Lajittele js tiedostot paremmin
+*/
+
+import { questions } from "./questions.js";
+import { shuffleArray } from "./shuffle.js";
+
+let score = 0;
+let currentQuestionIndex = 0;
+const hintsCount = 2;
+let hintsUsed = 0;
+let hintsLeft = 2;
+
+// Get html elements and store them
+const questionElement = document.getElementById("question");
+const nextButton = document.getElementById("next-question");
+const hintButton = document.getElementById("hint");
+const questionImage = document.getElementById("question-image");
+
+// Apply eventlisteners for the buttons
+nextButton.addEventListener("click", showQuestion);
+hintButton.addEventListener("click", applyHint);
+
+// Get all answer buttons and convert them into a normal array from nodelist using spread
+let answerButtonsNode = document.querySelectorAll(".answer");
+let answerButtons = [...answerButtonsNode];
+
+// Create a shuffled copy of the questions array
+let shuffledQuestions = shuffleArray(questions);
+
+//-----------------------------------------------------------//
+
+function showQuestion() {
+    // Remove the next question button at the start of each question
+    nextButton.style.display = "none";
+
+    // Remove styling from buttons from the previous questions
+    answerButtons.forEach((button) => {
+        button.classList.remove("correct", "incorrect");
+        button.classList.remove("hint-used");
+        button.removeEventListener("click", clickHandler);
+    });
+
+    // Get the current question and display it
+    if (currentQuestionIndex < 11) {
+        let currentQuestion = shuffledQuestions[currentQuestionIndex];
+        let questionNumber = currentQuestionIndex + 1;
+        questionElement.innerHTML = `${questionNumber}. ${currentQuestion.question}`;
+
+        // Change the questions image based on the question
+        questionImage.src = currentQuestion.img;
+
+        // Display all the possible answers and add eventlistener to each button
+        for (let i = 0; i < 4; i++) {
+            answerButtons[i].innerHTML = currentQuestion.answers[i].text;
+            answerButtons[i].addEventListener("click", clickHandler);
+        }
+
+        currentQuestionIndex++;
+    } else {
+        // All questions have been asked
+        questionElement.innerHTML = "All questions have been asked!";
+    }
+}
+
+function checkAnswer(selectedIndex, currentQuestion) {
+    // If the answer is correct
+    if (currentQuestion.answers[selectedIndex].correct === "true") {
+        score++;
+        answerButtons[selectedIndex].classList.add("correct");
+
+        // If the answer is incorrect
+    } else {
+        answerButtons[selectedIndex].classList.add("incorrect");
+
+        // Display the correct answer if the chosen answer was wrong
+        for (let i = 0; i < 4; i++) {
+            if (currentQuestion.answers[i].correct === "true") {
+                answerButtons[i].classList.add("correct");
+                break;
+            }
+        }
+    }
+
+    // Display the next question button after the answer has been given
+    nextButton.style.display = "block";
+}
+
+function applyHint() {
+    if (hintsUsed < hintsCount) {
+        // Get the current question
+        const currentQuestion = shuffledQuestions[currentQuestionIndex - 1];
+
+        // An empty array to store the incorrect answers
+        const answersToRemove = [];
+
+        //  While arrays length is less than 2, generate random index
+        while (answersToRemove.length < 2) {
+            const randomIndex = Math.floor(
+                Math.random() * currentQuestion.answers.length
+            );
+
+            // Get random answer from the current question
+            const randomAnswer = currentQuestion.answers[randomIndex];
+
+            // If the answer is not in the array yet and it is incorrect answer, then store it
+            if (!answersToRemove.includes(randomIndex)) {
+                if (randomAnswer.correct === "false") {
+                    answersToRemove.push(randomIndex);
+                }
+            }
+        }
+
+        // Loop trough the array of incorrect answers and remove 2 answers
+        for (let i = 0; i < answersToRemove.length; i++) {
+            let removeAnswerIndex = answersToRemove[i];
+
+            answerButtons[removeAnswerIndex].classList.add("hint-used");
+        }
+
+        hintsUsed++;
+        hintsLeft--;
+
+        // Display the current status of the hints
+        hintButton.innerHTML = `💡Hints left: ${hintsLeft}`;
+    }
+}
+
+// Own function for the answer clicking so that evenlisteners work as they should
+function clickHandler() {
+    // Get the index of the button that was pressed
+    const selectedIndex = answerButtons.indexOf(event.target);
+    // Get the current question
+    const currentQuestion = shuffledQuestions[currentQuestionIndex - 1];
+    // Call checkAnswer with previous information
+    checkAnswer(selectedIndex, currentQuestion);
+}
